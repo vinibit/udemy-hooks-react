fd89f5d9253b2a5680349eba4503c6d9008125e0
Vinicius Martins <vinibitmartins@gmail.com>
2024-11-06 17:41:34 -0300

    Implement UseCallback component with memoized increment function and add UseCallbackButtons for interaction

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButtons from './UseCallbackButtons'
 
 const UseCallback = (props) => {
+
+    const [count, setCount] = useState(0)
+    const increment = useCallback(delta => {
+        setCount(curr => curr + delta)
+    }, [setCount])
+    
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+
+            <div className="center">
+                <span className="text">{count}</span>                
+                <UseCallbackButtons increment={increment} />                
+            </div>
         </div>
     )
 }
