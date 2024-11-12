c17253c8009788c557f36dc292e12ae7649a3ffd
Vinicius Martins <vinibitmartins@gmail.com>
2024-11-12 20:10:48 -0300

    Add custom hooks for counter and data fetching, and update UseCustom component for interaction

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,15 +1,55 @@
 import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import PageTitle from 'components/layout/PageTitle'
+import SectionTitle from 'components/layout/SectionTitle'
+import { useCounter } from 'hooks/useCounter'
+import { useFetch } from 'hooks/useFetch'
 
-const UseRef = (props) => {
+const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+
+const UseCustom = (props) => {
+
+    const {count, increment, decrement, reset} = useCounter(0)
+
+    // fetch('http://files.cod3r.com.br/curso-react/estados.json')
+    //     .then(res => res.json())
+    //     .then(json => console.log(json)) 
+    const res = useFetch(url, 'get')
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+
+            <SectionTitle title="Exercício 01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn"
+                        onClick={() => increment()}>+</button>
+                    <button className="btn"
+                        onClick={() => decrement()}>-</button>
+                    <button className="btn"
+                        onClick={() => reset()}>Reset</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercício 02" />
+            <div className="left">
+                <ul style={{ 
+                        listStyleType: 'none',
+                        padding: 0, 
+                    }}>
+                    {
+                        !res.loading ? 
+                            res.data.map((state, index) => <li key={index}>{state.nome} - {state.sigla}</li>) 
+                            : false
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
 
-export default UseRef
+export default UseCustom
